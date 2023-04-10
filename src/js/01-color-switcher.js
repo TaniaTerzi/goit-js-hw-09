9dd10c6ec366d34aa3ecc03d29ead754cf940478
TaniaTerzi <terzitetyana@gmail.com>
2023-04-10 20:00:41 +0200

    Summary

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,32 @@
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+ //підключитися на кнопки
+  const startButton = document.querySelector('button[data-start]')
+  console.log(startButton);
+
+  const stopButton = document.querySelector('button[data-stop]')
+  console.log(stopButton);
+
+
+//слухач на кнопку
+//функція, яка змінює колір боді
+//підключити функцію зміни кольору до кнопки старт з інтервалом 1 сек.
+
+  startButton.addEventListener("click", () => {
+    timerId = setInterval(() => {
+      console.log(`interval start`);
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startButton.disabled = true;
+  });
+  
+  //кнопка стоп повинна зупиняти зміни кольорів
+  
+  stopButton.addEventListener("click", () => {
+    clearInterval(timerId);
+    console.log(`interval stop`);
+    startButton.disabled = false;
+  });
